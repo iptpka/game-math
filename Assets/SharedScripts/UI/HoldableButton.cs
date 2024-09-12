d84f8dffa6213d86587e338741c5ba7bf5faafba
iptpka <tuomasjarvenkyla@gmail.com>
2024-09-12 12:51:17 +0300

    Holdable button OnIsHeldChanged

--- Assets/SharedScripts/UI/HoldableButton.cs
+++ Assets/SharedScripts/UI/HoldableButton.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace GameMath.UI
@@ -6,17 +7,21 @@ namespace GameMath.UI
     public class HoldableButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         private bool isPointerDown;
-        
+
         public bool IsHeldDown => isPointerDown;
-        
+
+        public UnityEvent<bool> OnIsHeldChanged;
+
         public void OnPointerDown(PointerEventData eventData)
         {
             isPointerDown = true;
+            OnIsHeldChanged.Invoke(IsHeldDown);
         }
-        
+
         public void OnPointerUp(PointerEventData eventData)
         {
             isPointerDown = false;
+            OnIsHeldChanged.Invoke(IsHeldDown);
         }
     }
 }
