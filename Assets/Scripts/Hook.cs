a47d6b0b8a902c35eaeedc3be6be454db28edd7b
iptpka <tuomas.jarvenkyla@edu.turkuamk.fi>
2024-09-13 18:35:31 +0300

    Hook alignment

--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -5,10 +5,14 @@ namespace GameMath.Crane
 {
     public class Hook : Parentable
     {
+        [SerializeField] private Transform _crane;
         override protected void LateUpdate()
         {
             var delayedPosition = Vector3.Lerp(transform.position, GetTargetPosition(), 5f * Time.deltaTime);
             var delayedRotation = Quaternion.Lerp(transform.rotation, GetTargetRotation(), 5f * Time.deltaTime);
+            var trolleyDirection = _parent.position - transform.position;
+            var forward = transform.position - _crane.position;
+            delayedRotation.SetLookRotation(Vector3.Cross(forward, trolleyDirection), trolleyDirection);
             transform.SetPositionAndRotation(delayedPosition, delayedRotation);
         }
     }
