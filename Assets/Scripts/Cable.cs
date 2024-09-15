8c2b0ec8dd4eb8131825e74d1853228ddee3ed01
iptpka <tuomasjarvenkyla@gmail.com>
2024-09-15 18:41:21 +0300

    Proper cable

--- Assets/Scripts/Cable.cs
+++ Assets/Scripts/Cable.cs
@@ -4,18 +4,25 @@ namespace GameMath.Crane
 {
     public class Cable : MonoBehaviour
     {
-        [SerializeField] private Transform _trolley;
-        [SerializeField] private Transform _hook;
+        [SerializeField] private Transform _start;
+        [SerializeField] private Transform _end;
         private LineRenderer _lineRenderer;
+        private Vector3 _relativeToStart;
+        private Vector3 _relativeToEnd;
+
 
         void Awake()
         {
             _lineRenderer = GetComponent<LineRenderer>();
+            Vector3[] positions = new Vector3[2];
+            _lineRenderer.GetPositions(positions);
+            _relativeToStart = _start.InverseTransformPoint(positions[0]);
+            _relativeToEnd = _end.InverseTransformPoint(positions[1]);
         }
 
         void LateUpdate()
         {
-            _lineRenderer.SetPositions(new[] { _trolley.position, _hook.position });
+            _lineRenderer.SetPositions(new[] { _start.TransformPoint(_relativeToStart), _end.TransformPoint(_relativeToEnd) });
         }
     }
 }
